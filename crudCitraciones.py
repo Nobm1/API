5620a0bd3f8f1ffd91e1c1ddcbc4da5e50e3ee36
Nobm1 <74340698+Nobm1@users.noreply.github.com>
2024-08-07 14:28:51 -0400

    Update crudCitraciones.py

--- crudCitraciones.py
+++ crudCitraciones.py
@@ -536,8 +536,5 @@ async def actualizar_estado(id_usuario: int, ids_usuario:str, modificación: str
         print()
         return {"message": "Datos Ingresados Correctamente"}
     except Exception as e: raise HTTPException(status_code=500, detail=str(e))
-
-
-
 if __name__ == "__main__":
  uvicorn.run(app, host="0.0.0.0", port=8000)
